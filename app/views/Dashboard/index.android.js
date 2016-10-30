ee670f3012a80b3631c3db46a311b6768665a58f
Andy Justice <Andy Justice>
2016-10-30 00:34:21 -0700

    Router Functional, Basic View Modules Setup

--- app/views/Dashboard/index.android.js
+++ app/views/Dashboard/index.android.js
@@ -1,37 +1,51 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Navigator, TouchableHighlight } from 'react-native';
 import _ from 'lodash'
 
 import DashboardItem from '../DashboardItem/index.android.js'
-
-const stats = {
-  date: "10/29/16",
-  service: 25,
-  serviceCommision: 0.50,
-  sales: 100,
-  salesCommision: 0.40,
-  tips: 25,
-  tipAverage: 6,
-  hours: {
-    start: "14:30",
-    end: "18:30"
-  },
-  dollarPerHour: 12.50,
-  totalDailyRevenue: 250
-}
-
+import Login from '../Login/index.android.js'
+let state = {
+      date: "10/29/16",
+      service: 25,
+      serviceCommision: 0.50,
+      sales: 100,
+      taxRate: 0.40,
+      salesCommision: 0.40,
+      tips: 25,
+      tipAverage: 6,
+      hours: {
+        start: "14:30",
+        end: "18:30"
+      },
+      dollarPerHour: 12.50,
+      totalDailyRevenue: 250
+};
 class Dashboard extends Component {
+
+ _navigate(name) {
+  	this.props.navigator.push({
+    	name: name
+    })
+  }
   render() {
     return (
       <View>
-        <DashboardItem text={'Total Sales'} value="{stats.sales}"></DashboardItem>
-        <DashboardItem text={'Total Service'} value="{stats.service}">></DashboardItem>
-        <DashboardItem text={'Total Tips'} value="{stats.tips}">></DashboardItem>
+        <TouchableHighlight onPress={ () => this._navigate('Login')}>
+          <View>
+            <DashboardItem text={'Total Sales'} value="{stats.sales}"></DashboardItem>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={ () => this._navigate('DailySales')}>
+          <View>
+            <DashboardItem text={'Total Sales'} value="{stats.sales}"></DashboardItem>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
--- index.android.js
+++ index.android.js
@@ -1,20 +1,39 @@
 'use strict';
-
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Navigator, TouchableHighlight } from 'react-native';
+
 import Dashboard from './app/views/Dashboard/index.android.js'
 import NewSale from './app/views/NewSale/index.android.js'
 import ViewContainer from './app/views/ViewContainer.js'
+import Login from './app/views/Login/index.android.js'
+import ResetPassword from './app/views/ResetPassword/index.android.js'
+
 
 class TipCalc extends Component {
   render() {
     return (
-        <ViewContainer>
-          <Dashboard></Dashboard>
-        </ViewContainer>
+      <Navigator
+           initialRoute={{name: 'Main'}}
+           renderScene={(route, navigator) => {
+             switch(route.name){
+               case "Main":
+               return <Dashboard title={route.title} navigator={navigator} {...route.passProps} />
+               case "Login":
+               return <Login title={route.title} navigator={navigator} {...route.passProps} />
+               case "ResetPassword":
+               return <ResetPassword title={route.title} navigator={navigator} {...route.passProps} />
+               case "CreateAccount":
+               return <CreateAccount title={route.title} navigator={navigator} {...route.passProps} />
+               case "NewSale":
+               return <NewSale title={route.title} navigator={navigator} {...route.passProps} />
+             }
+           }}
+         />
     );
   }
+
 }
+
 
 const styles = StyleSheet.create({
   bigblue: {
